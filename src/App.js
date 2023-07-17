e8c69396e309b0edcee39de24629ccfeb64cde42
votri96 <votri96@gmail.com>
2023-07-18 05:07:06 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.scss';
+import logo from './logo.svg';
 
 function App() {
-  return <div className="app">Learn ReactJS - Easy Frontend</div>;
+  return (
+    <div className="app">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
