957d91b1f79f2b581abeefd7ca0ed1e16239e8ea
votri96 <votri96@gmail.com>
2023-07-19 01:24:22 +0700

    Test call api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import { NavLink, Redirect, Route, Switch } from 'react-router-dom';
 import AlbumFeature from './features/Album';
 import TodoFeature from './features/Todo';
 import NotFound from './components/NotFound';
+import productApi from './api/productApi';
 
 function App() {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const params = {
+        _limit: 10,
+      };
+      const productList = await productApi.getAll(params);
+      console.log(productList);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="App">
       <p>
