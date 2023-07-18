e52b18de0b61ada8aa9215b221752a904f9fba17
votri96 <votri96@gmail.com>
2023-07-18 12:16:28 +0700

    Display list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.scss';
-import logo from './logo.svg';
+import AlbumFeature from './features/Album';
+import TodoFeature from './features/Todo';
 
 function App() {
   return (
-    <div className="app">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="App">
+      <AlbumFeature />
+      <TodoFeature />
     </div>
   );
 }
