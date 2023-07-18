6dfe73a0d6236993555d1f8d076052b401493e60
votri96 <votri96@gmail.com>
2023-07-18 23:35:03 +0700

    Add route and navlink

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.scss';
+import { NavLink, Route, Switch } from 'react-router-dom';
+import AlbumFeature from './features/Album';
 import TodoFeature from './features/Todo';
 
 function App() {
   return (
     <div className="App">
-      <TodoFeature />
+      <p>
+        <NavLink to="/todos" activeClassName="active-menu">
+          Todos
+        </NavLink>
+      </p>
+      <p>
+        <NavLink to="/albums" activeClassName="active">
+          Albums
+        </NavLink>
+      </p>
+
+      <Switch>
+        <Route path="/" component={TodoFeature} exact />
+        <Route path="/todos" component={TodoFeature} />
+        <Route path="/albums" component={AlbumFeature} />
+      </Switch>
     </div>
   );
 }
